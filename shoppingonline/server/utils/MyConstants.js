936b2461aaa8dfa91d4a17b8cb5f3fcebc33f71f
NamDuongsss <dnam4673@gmail.com>
2023-07-13 13:15:18 +0700

    init project

---
+++ shoppingonline/server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'duong.lsrwtzz.mongodb.net',
+  DB_USER: 'duong',
+  DB_PASS: '23012002',
+  DB_DATABASE: 'shoppingonline',
+  EMAIL_USER: '<email_user>', // Microsoft mail service
+  EMAIL_PASS: '<email_pass>',
+  JWT_SECRET: 'duong01',
+  JWT_EXPIRES: '600000', // in milliseconds
+};
+module.exports = MyConstants;
